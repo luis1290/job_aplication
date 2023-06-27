3140975595282f3647590939054f0067dda00f0d
Luis <lgomez1290@gmail.com>
2023-06-26 23:42:59 -0600

    recruiter en progreso

---
+++ src/services/recruiters.services.js
@@ -0,0 +1,11 @@
+
+class recruiterServices {
+  static async createRecruiter(dataIntervies) {
+    try {
+  
+    } catch (error) {
+       throw error
+    }
+  }
+}
+module.exports = recruiterServices
